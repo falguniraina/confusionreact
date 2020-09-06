c7c38b912a1fbe8ca688b72d63f1b83f638f326f
falguniraina <rainadiv01@gmail.com>
2020-09-06 16:50:12 +0530

    Components Part 1&2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React,{Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import { DISHES } from './shared/dishes';
+
+import Menu from './components/MenuComponent';
+
+
 
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +24,8 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        
+  <Menu dishes={this.state.dishes} />
       </div>
     );
   }
